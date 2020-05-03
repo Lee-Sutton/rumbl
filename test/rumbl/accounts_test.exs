ca518c990180c3b9f1dde82a028cbcc3b70f128f
Lee Sutton <leesutton1@gmail.com>
2020-05-03 14:39:45 -0700

    added tests for auth by username/password

--- test/rumbl/accounts_test.exs
+++ test/rumbl/accounts_test.exs
@@ -10,45 +10,66 @@ defmodule Rumble.AccountsTests do
     }
 
     @invalid_attrs %{}
+
+    test "valid data inserts user" do
+      {:ok, user} = Accounts.register_user(@valid_attrs)
+      assert user.id
+      assert user.name == "User"
+      assert user.username == "eva"
+      assert user.password
+      assert user.password_hash
+    end
+
+    test "invalid data does not insert user" do
+      {:error, _} = Accounts.register_user(@invalid_attrs)
+
+      assert Accounts.list_users() == []
+    end
+
+    test "enforce unique username" do
+      {:ok, user} = Accounts.register_user(@valid_attrs)
+      assert user.id
+
+      {:error, changeset} = Accounts.register_user(@valid_attrs)
+
+      assert %{username: ["has already been taken"]} = errors_on(changeset)
+      assert [user] = Accounts.list_users()
+    end
+
+    test "does not accept long usernames" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+
+      {:error, changeset} = Accounts.register_user(attrs)
+      assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
+    end
+
+    test "requires password to be at least 6 characters" do
+      attrs = Map.put(@valid_attrs, :password, String.duplicate("a", 5))
+
+      {:error, changeset} = Accounts.register_user(attrs)
+      assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
+    end
   end
 
-  test "valid data inserts user" do
-    {:ok, user} = Accounts.register_user(@valid_attrs)
-    assert user.id
-    assert user.name == "User"
-    assert user.username == "eva"
-    assert user.password
-    assert user.password_hash
-  end
+  describe "authenticate_by_username_and_pass/2" do
+    @pass "123456"
 
-  test "invalid data does not insert user" do
-    {:error, _} = Accounts.register_user(@invalid_attrs)
+    setup do
+      {:ok, user: user_fixture(password: @pass)}
+    end
 
-    assert Accounts.list_users() == []
-  end
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} = Accounts.authenticate_by_username_and_pass(user.username, @pass)
+      assert auth_user.id == user.id
+    end
 
-  test "enforce unique username" do
-    {:ok, user} = Accounts.register_user(@valid_attrs)
-    assert user.id
+    test "returns anauthorized error for invalid password", %{user: user} do
+      assert {:error, :unauthorized} = Accounts.authenticate_by_username_and_pass(user.username, "badpassword")
+    end
 
-    {:error, changeset} = Accounts.register_user(@valid_attrs)
-
-    assert %{username: ["has already been taken"]} = errors_on(changeset)
-    assert [user] = Accounts.list_users()
-  end
-
-  test "does not accept long usernames" do
-    attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
-
-    {:error, changeset} = Accounts.register_user(attrs)
-    assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
-  end
-
-  test "requires password to be at least 6 characters" do
-    attrs = Map.put(@valid_attrs, :password, String.duplicate("a", 5))
-
-    {:error, changeset} = Accounts.register_user(attrs)
-    assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
+    test "returns not found with no matching user" do
+      assert {:error, :not_found} = Accounts.authenticate_by_username_and_pass("invalid", "badpassword")
+    end
     
   end
 end
